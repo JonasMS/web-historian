9aaf77f9084e4f4ad1c70d2a1a4580334df03234
Matthew Vargeson <mvargeson@Matthews-MacBook-Air.local>
2016-05-11 12:11:01 -0700

    serve static files

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -2,6 +2,8 @@ var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 
+exports.docRoot = 'web/public';
+
 exports.headers = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -10,12 +12,30 @@ exports.headers = {
   'Content-Type': 'text/html'
 };
 
+
+exports.notFound = function(res) {
+  res.writeHead(404);
+  res.end('Not Found');
+};
+
+exports.notImplemented = function(res) {
+  res.writeHead(501);
+  res.end('Not Implemented');
+};
+
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  fs.readFile(exports.docRoot + pathname, (err, contents) => {
+    if (err) {
+      exports.notFound(res);
+    } else {
+      res.writeHead(200);
+      res.end(contents);
+    }
+    callback();
+  });
 };
-
-
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,24 @@
+var url = require('url');
 var path = require('path');
+var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  pathname = url.parse(req.url).pathname.replace(/\/$/, '');
+  switch (pathname) {
+  case '':
+    if (req.method === 'GET') {
+      pathname = '/index.html';
+      helpers.serveAssets(res, pathname, () => {});
+    } else if (req.method === 'POST') { 
+      console.log(req);
+    } else {
+      notImplemented(res);
+    }
+    break;
+  default:
+    helpers.serveAssets(res, pathname, () => {});
+  }
 };
