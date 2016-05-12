6cd4b791d47466dea3a51e0a8c7ea680d7cfcbd8
jonasms <jonas.sota@gmail.com>
2016-05-12 12:25:02 -0700

    Develop worker

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -2,8 +2,29 @@
 // that are waiting.
 var archive = require('../helpers/archive-helpers');
 
-archive.readListOfUrls(list => {
-  archive.isUrlArchived(list, () => {
 
+//get list of urls in sites.txt
+
+//determine which urls in sites.txt need to be fetched
+
+//fetch urls
+
+
+archive.readListOfUrls(list => {
+  var filteredUrls = [];
+  var urlCheckCount = 0;
+  list.forEach(item => {
+    archive.isUrlArchived(item, exists => {
+      urlCheckCount++;
+      if (!exists) { filteredUrls.push(item); }
+      console.log('line 18: ', filteredUrls);
+      if (urlCheckCount === list.length) {
+        archive.downloadUrls(filteredUrls);
+      }
+    });
   });
 });
+
+
+  
+
